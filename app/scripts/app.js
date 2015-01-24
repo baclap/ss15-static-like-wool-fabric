652ddacf7949a778d232bebb965402abf751c75b
baclap <clapperbrett@gmail.com>
2015-01-23 19:06:52 -0800

    Create header and content area, place logo in header, delete a few yo generated things

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 (function (document) {
-  'use strict';
+    'use strict';
 
-  document.addEventListener('polymer-ready', function () {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
-  });
+    document.addEventListener('polymer-ready', function () {
+        // Perform some behaviour
+        console.log('Polymer is ready to rock!');
+    });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
